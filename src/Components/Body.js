145c53166c58285acefc9e059e8a4393ccfa14fd
vipulbadwaik <vipul.badwaik@gmail.com>
2023-12-30 02:58:32 +0530

    Header Done

---
+++ src/Components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='h-screen bg-slate-950'>Body</div>
+  )
+}
+
+export default Body
---
+++ src/Components/Header.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const changeHeader = () => {
+
+    console.log(window.scrollY);
+
+    if (window.scrollY > 70) {
+      setShow(true);
+      window.removeEventListener('scroll',changeHeader);
+    } else {
+      setShow(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeHeader);
+
+  return show ? (<div >
+    <div className="h-20 flex justify-center">
+    <div className="h-10 w-1/3 rounded-full bg-slate-600 flex text-white items-center justify-evenly fixed mt-5">
+   <h4 className="cursor-pointer">About</h4> |
+   <h4 className="cursor-pointer">Experience</h4> |
+   <h4 className="cursor-pointer">Projects</h4> |
+   <h4 className="cursor-pointer">Contact</h4>
+  </div>
+    </div>
+  
+
+</div>) : (
+    
+    <div className="bg-red-500 text-white flex h-20 justify-between  items-center ">
+      <div className="ml-5">Vipul Badwaik</div>
+      <div className="flex ">
+        <h4 className="m-4 p-2 hover:rounded-full hover:text-slate-950 hover:bg-red-300 hover:transition-all ease-in-out delay-100 cursor-pointer">
+          About
+        </h4>
+        <h4 className="m-4 p-2 hover:rounded-full hover:text-slate-950 hover:bg-red-300 hover:transition-all ease-in-out delay-100 cursor-pointer">
+          Project
+        </h4>
+        <h4 className="m-4 p-2 hover:rounded-full hover:text-slate-950 hover:bg-red-300 hover:transition-all ease-in-out delay-100 cursor-pointer">
+          Contact
+        </h4>
+      </div>
+    </div>
+   
+ 
+  )
+};
+
+export default Header;
